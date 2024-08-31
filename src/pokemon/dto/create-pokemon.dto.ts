1df83e75cb8870e5b4af1f988e034840c45d0f1e
Esteban Perea <estebanperea@gmail.com>
2024-08-31 11:43:54 -0300

    Fin seccion 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,14 @@
+import { IsPositive, IsString, Min, MinLength, IsInt } from "class-validator";
+
+export class CreatePokemonDto {
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+
+}
